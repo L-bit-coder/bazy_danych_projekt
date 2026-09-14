2fcb653b3bfe8a32e13a1b705505c025445b5463
lucek <u177_klaluc_waw@technischools.com>
2026-09-14 11:48:30 +0200

    czwarty commit

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import sqlite3
 con = sqlite3.connect("tutorial.db")
 cur = con.cursor()
+
+cur.execute("""CREATE TABEL IF NOT EXISTS USERS(
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            NAME VARCHAR(50) NOT NULL,
+            SURNAME VARCHAR(50) NOT NULL,
+            EMAIL VARCHAR(50) NOT NULL UNIQUE,
+            PHONE VARCHAR(50) NOT NULL,""")
