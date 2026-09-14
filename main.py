9ee5b0a4bf4c818469939b658e590f6614081910
lucek <u177_klaluc_waw@technischools.com>
2026-09-14 12:08:01 +0200

    szosty commit jednak nie final

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ cur.execute("""CREATE TABEL IF NOT EXISTS USERS(
             SURNAME VARCHAR(50) NOT NULL,
             EMAIL VARCHAR(50) NOT NULL UNIQUE,
             PHONE VARCHAR(50) NOT NULL,""")
+cur.execute("""CREATE TABEL IF NOT EXISTS transactions(
+               ID INTEGER PRIMARY KEY AUTOINCREMENT,
+               amount decimal(10,2) NOT NULL,
+               user_from_id INTEGER NOT NULL,
+               user_to_id INTEGER NOT NULL,
+               data VARCHAR NOT NULL,
+               FORGEIN KEY (user_from_id) REFERENCES USERS(ID),
+               FORGEIN KEY (user_to_id) REFERENCES transactions(ID),
+               """)
